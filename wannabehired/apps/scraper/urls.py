d24b91bf291ad7dfa37d2bdd0433371aa57b5834
ankitjavalkar <ankitjavalkar@users.noreply.github.com>
2020-05-11 10:59:48 +0530

    Add urls

---
+++ wannabehired/apps/scraper/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.jobs),
+    # path('articles/<int:year>/', views.year_archive),
+    # path('articles/<int:year>/<int:month>/', views.month_archive),
+    # path('articles/<int:year>/<int:month>/<slug:slug>/', views.article_detail),
+]
