bee1b0391af284fa1f47f528109d74081577879b
ankitjavalkar <ankitjavalkar@users.noreply.github.com>
2020-05-11 10:59:14 +0530

    Modify the views to display a list of jobs for the latest thread

--- wannabehired/apps/scraper/views.py
+++ wannabehired/apps/scraper/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
+from .models import Thread, Job
+
 def jobs(request):
-	thread = Thread.objects.all().order_by('-created_on').first()
-	jobs = job_list.jobs_set.all()
-	return render(request, 'apps/scraper/list.html', {'jobs': jobs})
+    thread = Thread.objects.all().order_by('created_on').first()
+    jobs = thread.job_set.all()
+    return render(request, 'scraper/list.html', {'jobs': jobs})
